e931608ff6d29c27abaed4b3ecd661069be01be0
paul de wouters <paul@hmn.md>
2015-01-14 10:21:50 -0300

    Fix option name

--- backupwordpress-pro-ftp.php
+++ backupwordpress-pro-ftp.php
@@ -4,7 +4,7 @@ Plugin Name: BackUpWordPress to FTP
 Plugin URI: https://bwp.hmn.md/downloads/backupwordpress-to-ftp/
 Description: Send your backups to your Dropbox account
 Author: Human Made Limited
-Version: 2.0
+Version: 2.0.3
 Author URI: https://bwp.hmn.md/
 License: GPLv2
 Network: true
@@ -43,7 +43,7 @@ class BackUpWordPress_FTP {
 	/**
 	 * The plugin version number.
 	 */
-	const PLUGIN_VERSION = '2.0';
+	const PLUGIN_VERSION = '2.0.3';
 
 	/**
 	 * Minimum version of BackUpWordPress compatibility.
@@ -203,7 +203,7 @@ class BackUpWordPress_FTP {
 	 * @return array
 	 */
 	public function fetch_settings() {
-		return array_merge( $this->default_settings(), get_option( 'hmbkpp_dx_settings', array() ) );
+		return array_merge( $this->default_settings(), get_option( 'hmbkpp_ftp_settings', array() ) );
 	}
 
 	/**
